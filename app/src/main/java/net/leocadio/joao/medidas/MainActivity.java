57d67e2dccaa4c2399f4b5801392fc492513faa7
Joao <juaorik@gmail.com>
2018-09-19 14:25:02 -0300

    update
    update

--- app/src/main/java/net/leocadio/joao/medidas/MainActivity.java
+++ app/src/main/java/net/leocadio/joao/medidas/MainActivity.java
@@ -7,6 +7,14 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 public class MainActivity extends Activity {
 
     private Medidas selectedEditTxt;
@@ -18,6 +26,28 @@ public class MainActivity extends Activity {
 
         final EditText centimetro = (EditText) findViewById(R.id.cmText);
         final EditText polegada = (EditText) findViewById(R.id.polText);
+        final BarChart barChart = (BarChart) findViewById(R.id.barChart);
+
+        barChart.setDrawBarShadow(false);
+        barChart.setDrawValueAboveBar(true);
+        barChart.setMaxVisibleValueCount(50);
+        barChart.setPinchZoom(false);
+        barChart.setDrawGridBackground(true);
+
+        ArrayList<BarEntry> barEntries = new ArrayList<>();
+
+        barEntries.add(new BarEntry(1, 40f));
+        barEntries.add(new BarEntry(2, 44f));
+        barEntries.add(new BarEntry(3, 30f));
+        barEntries.add(new BarEntry(4, 36f));
+
+        BarDataSet barDataSet = new BarDataSet(barEntries, "Medidas");
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        BarData data = new BarData(barDataSet);
+        data.setBarWidth(0.9f);
+
+        barChart.setData(data);
 
         // Centimentro
         centimetro.setOnFocusChangeListener(new View.OnFocusChangeListener() {
