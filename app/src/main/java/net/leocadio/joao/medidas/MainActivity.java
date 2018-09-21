dbb90949b5edc28da0e6d86225ae0d150365e8f4
Joao <juaorik@gmail.com>
2018-09-21 20:47:24 -0300

    update
    update

--- app/src/main/java/net/leocadio/joao/medidas/MainActivity.java
+++ app/src/main/java/net/leocadio/joao/medidas/MainActivity.java
@@ -1,15 +1,12 @@
 package net.leocadio.joao.medidas;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 
-import me.ithebk.barchart.BarChart;
-import me.ithebk.barchart.BarChartModel;
 
 public class MainActivity extends Activity {
 
@@ -22,24 +19,8 @@ public class MainActivity extends Activity {
 
         final EditText centimetro = (EditText) findViewById(R.id.cmText);
         final EditText polegada = (EditText) findViewById(R.id.polText);
-        final BarChart barChart = (BarChart) findViewById(R.id.barChart);
+//        final BarChart barChart = (BarChart) findViewById(R.id.barChart);
 
-        //Add single bar
-        barChart.setBarMaxValue(999);
-
-        BarChartModel barCentimetro = new BarChartModel();
-        barCentimetro.setBarValue(0);
-        barCentimetro.setBarColor(Color.parseColor("#FFC107"));
-        barCentimetro.setBarTag("Centimetros"); //You can set your own tag to bar model
-        barCentimetro.setBarText("Centimetros");
-        barChart.addBar(barCentimetro);
-
-        BarChartModel barPolegada = new BarChartModel();
-        barPolegada.setBarValue(0);
-        barPolegada.setBarColor(Color.parseColor("#28A745"));
-        barPolegada.setBarTag("Polegadas"); //You can set your own tag to bar model
-        barPolegada.setBarText("Polegadas");
-        barChart.addBar(barPolegada);
 
         // Centimentro
         centimetro.setOnFocusChangeListener(new View.OnFocusChangeListener() {
@@ -61,26 +42,6 @@ public class MainActivity extends Activity {
                     float outputPol = (float) (temp * 0.39370);
 
                     polegada.setText(String.valueOf(stripDecimal(outputPol)));
-
-                    int convertCM = Math.round(temp);
-
-                    BarChartModel barCentimetro = new BarChartModel();
-                    barCentimetro.setBarValue(convertCM);
-                    barCentimetro.setBarColor(Color.parseColor("#28A745"));
-                    barCentimetro.setBarTag("Centimetros"); //You can set your own tag to bar model
-                    barCentimetro.setBarText("Centimetros");
-                    barChart.updateBar(convertCM, barCentimetro);
-                    barChart.getBar();
-
-                    int convertPol = Math.round(outputPol);
-
-                    BarChartModel barPolegada = new BarChartModel();
-                    barPolegada.setBarValue(convertPol);
-                    barPolegada.setBarColor(Color.parseColor("#28A745"));
-                    barPolegada.setBarTag("Polegadas"); //You can set your own tag to bar model
-                    barPolegada.setBarText("Polegadas");
-                    barChart.updateBar(convertPol, barPolegada);
-                    barChart.getBar();
 
                 } else if (selectedEditTxt == Medidas.CENTIMENTROS){
                     polegada.setText("");
@@ -110,7 +71,7 @@ public class MainActivity extends Activity {
                 if (!tmpValue.equals("") && !tmpValue.equals(".") && selectedEditTxt == Medidas.POLEGADAS) {
                     float temp = Float.parseFloat(tmpValue);
                     //centimetros
-                    final float outputCent = (float) (temp / 0.39370);
+                    float outputCent = (float) (temp / 0.39370);
 
                     centimetro.setText(String.valueOf(stripDecimal(outputCent)));
 
---
+++ readme.md
@@ -0,0 +1,4 @@
+
+## Android - Inch to Cm and vice-versa
+
+![pagina index](https://raw.githubusercontent.com/joaorik/android-medidas/master/index.png)
